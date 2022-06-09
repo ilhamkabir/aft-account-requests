29f40a1f76aaa93f2f846e1996682e1caeaff99a
ilhamkabir <ilham.kabir@gmail.com>
2022-06-09 09:39:42 -0600

    restructure code

---
+++ terraform/main.tf
@@ -0,0 +1,27 @@
+module "sandbox" {
+  source = "./modules/aft-account-request"
+
+  control_tower_parameters = {
+    AccountEmail              = "ilham.aws.sandbox@gmail.com"
+    AccountName               = "sandbox-aft"
+    ManagedOrganizationalUnit = "Sandbox"
+    SSOUserEmail              = "ilham.aws.sandbox@gmail.com"
+    SSOUserFirstName          = "Sandbox"
+    SSOUserLastName           = "AFT"
+  }
+
+  account_tags = {
+    "AFT" = "Sandbox"
+  }
+
+  change_management_parameters = {
+    change_requested_by = "Ilham Kabir"
+    change_reason       = "testing the account vending process"
+  }
+
+  custom_fields = {
+    group = "non-prod"
+  }
+
+  account_customizations_name = "sandbox"
+}
--- terraform/modules/aft-account-request/variables.tf
+++ terraform/modules/aft-account-request/variables.tf
@@ -1,6 +1,3 @@
-# Copyright Amazon.com, Inc. or its affiliates. All rights reserved.
-# SPDX-License-Identifier: Apache-2.0
-#
 variable "account-request-table" {
   type        = string
   description = "name of account-request-table"
@@ -34,7 +31,6 @@ variable "change_management_parameters" {
 variable "account_tags" {
   type        = map(any)
   description = "map of account-level tags"
-  default     = {}
 }
 
 variable "custom_fields" {
@@ -45,6 +41,7 @@ variable "custom_fields" {
 
 variable "account_customizations_name" {
   type        = string
-  default     = ""
+  default     = null
   description = "The name of the account customizations to apply"
 }
+
